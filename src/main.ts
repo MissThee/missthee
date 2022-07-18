9b641689b562d6686fb74e1a88d06792eae8fc73
MissThee <1163182715@qq.com>
2022-07-18 13:46:14 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,4 @@
 import './style/index.less'
 // import './listGenerator'
 
-let currentUITypeStep = 0
-let currentStartTimestamp = 0
-const currentSplitTime = 500
-
-const updateUIBehavior = () => {
-    if (currentStartTimestamp + currentSplitTime > performance.now() || currentUITypeStep >= 5) {
-        return
-    }
-    currentUITypeStep++
-    currentStartTimestamp = performance.now()
-    document.getElementById('content')?.classList.add(`content--type-${currentUITypeStep}`)
-}
-
-window.onload = () => {
-    const infoCardElArr = document.getElementsByClassName('info-card')
-    for (let i = 0; i < infoCardElArr.length; i++) {
-        infoCardElArr[i]?.addEventListener('mouseenter', updateUIBehavior)
-    }
-    document.addEventListener('touchstart', updateUIBehavior)
-}
 export {}
